103312866375a7b9795971a2cc8dfbbc3476278c
berkanankal <berkan.ankal@outlook.com>
2022-08-10 22:55:42 +0300

    routing & navigation

--- 2- northwind-app/src/app/app-routing.module.ts
+++ 2- northwind-app/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "products", component: ProductsComponent },
+  { path: "categories", component: CategoriesComponent },
+  { path: "employees", component: EmployeesComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
